433b63934e94e4bef1b7014500a1dd5d69566449
matheus <matheusfonseca5100@gmail.com>
2024-06-07 22:44:50 -0300

    feat: criacao de algumas rotas

--- Psychomeet/src/app/app.routes.ts
+++ Psychomeet/src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { SignInPatientComponent } from './views/account/sign-in-patient/sign-in-patient.component';
+import { SignUpPatientComponent } from './views/account/sign-up-patient/sign-up-patient.component';
 
-export const routes: Routes = [];
+import { HomeComponent } from './views/app/home/home.component';
+
+
+
+export const routes: Routes = [
+{
+    path: 'account/sign-in',
+    component: SignInPatientComponent,
+},
+{
+    path: 'account/sign-up',
+    component: SignUpPatientComponent,
+},
+{
+    path: 'app/home',
+    component: HomeComponent
+},
+{
+    path: '',
+    component: HomeComponent,
+}
+];
+

63f0f3cf791d727dc8e6d056df47b82ca97dbdac
matheus <matheusfonseca5100@gmail.com>
2024-06-08 23:14:59 -0300

    feat: criacao do arquivo ddl com base no mer atuaizado

---
+++ docs/Visão de Dados/psycomeet.sql
@@ -0,0 +1,112 @@
+drop database if exists psychomeet;
+create database psychomeet;
+
+\c psychomeet;
+
+begin;
+
+create table pessoa (
+    id serial primary key,
+    telefone varchar(15) not null unique,
+    nome varchar(100) not null,
+    senha varchar(50) not null,
+    data_nascimento date not null,
+    cpf varchar(14) not null unique,
+    email varchar(100) not null unique
+);
+
+create table foto (
+    id serial primary key,
+    pessoa_id int not null,
+    foto bytea not null,
+    constraint fk_pessoa_foto foreign key (pessoa_id) references pessoa(id),
+    constraint uk_pessoa_foto unique (pessoa_id, foto)
+);
+
+create table paciente (
+    id serial primary key,
+    pessoa_id int not null unique,
+    constraint fk_pessoa_id foreign key (pessoa_id) references pessoa(id),
+    constraint uk_paciente_pessoa_id unique (pessoa_id)
+);
+
+create table psicologo (
+    id serial primary key,
+    pessoa_id int not null unique,
+    crp varchar(20) not null unique,
+    descricao text not null,
+    constraint fk_pessoa_psicologo foreign key (pessoa_id) references pessoa(id),
+    constraint uk_psicologo_pessoa_id unique (pessoa_id),
+    constraint uk_psicologo_crp unique (crp)
+);
+
+create table publico (
+    id serial primary key,
+    psicologo_id int not null,
+    publico text not null,
+    constraint fk_psicologo_publico foreign key (psicologo_id) references psicologo(id),
+    constraint uk_psicologo_publico unique (psicologo_id, publico)
+);
+
+create table abordagem (
+    id serial primary key,
+    psicologo_id int not null,
+    abordagem text not null,
+    constraint fk_psicologo_abordagem foreign key (psicologo_id) references psicologo(id),
+    constraint uk_psicologo_abordagem unique (psicologo_id, abordagem)
+);
+
+create table especialidade (
+    id serial primary key,
+    descricao text not null,
+    constraint uk_especialidade_descricao unique (descricao)
+);
+
+create table disponibilidade (
+    id serial primary key,
+    psicologo_id int not null,
+    constraint fk_psicologo_disponibilidade foreign key (psicologo_id) references psicologo(id),
+    constraint uk_disponibilidade_psicologo_id unique (psicologo_id)
+);
+
+create table dia (
+    id serial primary key,
+    disponibilidade_id int not null,
+    turno varchar(50) not null,
+    dia_semana varchar(15) not null,
+    hora_inicio time not null,
+    hora_fim time not null,
+    constraint fk_disponibilidade_dia foreign key (disponibilidade_id) references disponibilidade(id),
+    constraint ck_dia_semana check (dia_semana in ('segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado', 'domingo')),
+    constraint uk_dia_disponibilidade_turno_dia_semana unique (disponibilidade_id, turno, dia_semana)
+);
+
+create table agenda (
+    id serial primary key,
+    data date not null,
+    hora_inicio time not null,
+    hora_fim time not null,
+    status varchar(50) not null,
+    constraint uk_agenda_data_hora unique (data, hora_inicio, hora_fim)
+);
+
+create table consulta (
+    id serial primary key,
+    agenda_id int not null unique,
+    relato text not null,
+    diagnostico text not null,
+    nota_paciente text not null,
+    comentario_paciente text,
+    constraint fk_agenda_consulta foreign key (agenda_id) references agenda(id),
+    constraint uk_consulta_agenda_id unique (agenda_id)
+);
+
+create table psicologo_especialidade (
+    psicologo_id int not null,
+    especialidade_id int not null,
+    constraint fk_psicologo_especialidade_psicologo foreign key (psicologo_id) references psicologo(id),
+    constraint fk_psicologo_especialidade_especialidade foreign key (especialidade_id) references especialidade(id),
+    primary key (psicologo_id, especialidade_id)
+);
+
+commit;

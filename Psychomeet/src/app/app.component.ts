87c5dbd1aa1830e9c627cf1bf48f8fdd7277b4f7
matheus <matheusfonseca5100@gmail.com>
2024-06-08 21:22:51 -0300

    feat: atualizacoes para entrega 2

--- Psychomeet/src/app/app.component.ts
+++ Psychomeet/src/app/app.component.ts
@@ -2,10 +2,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root', // O seletor do componente raiz
-  templateUrl: './app.component.html', // O arquivo HTML associado a este componente
-  styleUrls: ['./app.component.css'] // O arquivo CSS associado a este componente
+  selector: 'app-root', 
+  templateUrl: './app.component.html', 
+  styleUrls: ['./app.component.css'] 
 })
 export class AppComponent {
-  title = 'Psychomeet'; // Uma propriedade que pode ser usada no template HTML
+  title = 'Psychomeet'; 
 }

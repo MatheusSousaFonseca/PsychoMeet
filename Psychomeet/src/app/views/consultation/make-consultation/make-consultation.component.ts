ef5e9c4262494c1b21550af468953607f0c68f86
matheus <matheusfonseca5100@gmail.com>
2024-06-13 21:29:42 -0300

    feat: correcoes do frontend da tela make-consultation

--- Psychomeet/src/app/views/consultation/make-consultation/make-consultation.component.ts
+++ Psychomeet/src/app/views/consultation/make-consultation/make-consultation.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-make-consultation',
   templateUrl: './make-consultation.component.html',
   styleUrls: ['./make-consultation.component.css']
 })
-export class MakeConsultationComponent {
+export class MakeConsultationComponent implements OnInit {
+  ngOnInit(): void {
+    this.times = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'];
+  }
+
   days: string[] = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo'];
   times: string[] = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'];
 
-  isModalOpen: boolean = false;
+  isModalOpen: boolean = true;
   helpRequested: boolean = false;
   selectedTime: string | null = null;
   selectedDay: string | null = null;

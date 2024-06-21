73cbb3821e86f36fc8d9ae4d9f9966d91ae70d08
karen <karenpereira9680@gmail.com>
2024-06-21 11:06:55 -0300

    feat: adicao das telas de confirmar e cancelar consulta

--- Psychomeet/src/app/app.routes.ts
+++ Psychomeet/src/app/app.routes.ts
@@ -4,29 +4,39 @@ import { SignUpPatientComponent } from './views/account/sign-up-patient/sign-up-
 
 import { HomeComponent } from './views/app/home/home.component';
 import { MakeConsultationComponent } from './views/consultation/make-consultation/make-consultation.component';
+import { PopUpAcceptConsultationComponent } from './views/pop-ups/pop-up-accept-consultation/pop-up-accept-consultation.component';
+import { PopUpCancelConsultationComponent } from './views/pop-ups/pop-up-cancel-consultation/pop-up-cancel-consultation.component';
 
 
 
 export const AppRoutes: Routes = [
-{
-    path: 'account/sign-in-patient',
-    component: SignInPatientComponent,
-},
-{
-    path: 'account/sign-up-patient',
-    component: SignUpPatientComponent,
-},
-{
-    path: 'app/home',
-    component: HomeComponent
-},
-{
-    path: '',
-    component: HomeComponent,
-},
-{
-    path: 'consultation/make-consultation',
-    component: MakeConsultationComponent,
-}
+    {
+        path: 'account/sign-in-patient',
+        component: SignInPatientComponent,
+    },
+    {
+        path: 'account/sign-up-patient',
+        component: SignUpPatientComponent,
+    },
+    {
+        path: 'app/home',
+        component: HomeComponent
+    },
+    {
+        path: '',
+        component: HomeComponent,
+    },
+    {
+        path: 'consultation/make-consultation',
+        component: MakeConsultationComponent,
+    },
+    {
+        path: 'pop-ups/pop-up-accept-consultation',
+        component: PopUpAcceptConsultationComponent,
+    },
+    {
+        path: 'pop-ups/pop-up-cancel-consultation',
+        component: PopUpCancelConsultationComponent,
+    }
 ];
 

fac6c956c4e9efb69b90e4469e39223ffd113ca0
karen <karenpereira9680@gmail.com>
2024-06-20 22:04:16 -0300

    feat: correcoes sugeridas pelo Luaciano

--- Psychomeet/src/app/views/consultation/make-consultation/make-consultation.component.ts
+++ Psychomeet/src/app/views/consultation/make-consultation/make-consultation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Horario } from '../../../domain/model/horario-model';
 
 @Component({
   selector: 'app-make-consultation',
@@ -6,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./make-consultation.component.css']
 })
 export class MakeConsultationComponent implements OnInit {
+
+  constructor(private router: Router) {
+  }
+
   ngOnInit(): void {
     this.times = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'];
+
+    for (let i = 8; i < 18; i++) {
+
+      let horarios: Horario = {
+        hora: `${i}:00 - ${i + 1}:00`,
+        segunda: "Segunda-feira",
+        terca: "Terça-feira",
+        quarta: "Quarta-feira",
+        quinta: "Quinta-feira",
+        sexta: "Segunda-feira",
+        sabado: "Sabado",
+        domingo: "Segunda-feira",
+      };
+      this.horarioLista.push(horarios);
+    }
+
+
+
   }
+
+  horarioLista: Horario[] = [];
+
+
 
   days: string[] = ['Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado', 'Domingo'];
   times: string[] = ['08:00', '09:00', '10:00', '11:00', '12:00', '13:00', '14:00', '15:00', '16:00', '17:00'];
@@ -19,6 +47,8 @@ export class MakeConsultationComponent implements OnInit {
   selectedDay: string | null = null;
 
   openModal(time: string, day: string): void {
+    this.router.navigate(['/']);
+
     this.selectedTime = time;
     this.selectedDay = day;
     this.isModalOpen = true;
@@ -42,5 +72,6 @@ export class MakeConsultationComponent implements OnInit {
     this.closeModal();
   }
 
-  
+
 }
+

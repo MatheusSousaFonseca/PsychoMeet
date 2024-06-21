1eb2588354bb042667ffa23e68b04964cdb9f92f
karen <karenpereira9680@gmail.com>
2024-06-21 15:10:42 -0300

    feat: finalizacao das rotas

--- Psychomeet/src/app/app.routes.ts
+++ Psychomeet/src/app/app.routes.ts
@@ -7,6 +7,13 @@ import { MakeConsultationComponent } from './views/consultation/make-consultatio
 import { PopUpAcceptConsultationComponent } from './views/pop-ups/pop-up-accept-consultation/pop-up-accept-consultation.component';
 import { PopUpCancelConsultationComponent } from './views/pop-ups/pop-up-cancel-consultation/pop-up-cancel-consultation.component';
 import { PopUpRefuseConsultationComponent } from './views/pop-ups/pop-up-refuse-consultation/pop-up-refuse-consultation.component';
+import { ViewConsultationPatientComponent } from './views/consultation/view-consultation-patient/view-consultation-patient.component';
+import { ViewConsultationPsychologistComponent } from './views/consultation/view-consultation-psychologist/view-consultation-psychologist.component';
+import { ViewRequestConsultationComponent } from './views/consultation/view-request-consultation/view-request-consultation.component';
+import { PopUpGiveFeedbackComponent } from './views/pop-ups/pop-up-give-feedback/pop-up-give-feedback.component';
+import { PopUpInformAvailabilityComponent } from './views/pop-ups/pop-up-inform-availability/pop-up-inform-availability.component';
+import { PopUpMakeConsultationComponent } from './views/pop-ups/pop-up-make-consultation/pop-up-make-consultation.component';
+import { PopUpRescheduleConsultationComponent } from './views/pop-ups/pop-up-reschedule-consultation/pop-up-reschedule-consultation.component';
 
 
 
@@ -32,12 +39,40 @@ export const AppRoutes: Routes = [
         component: MakeConsultationComponent,
     },
     {
+        path: 'consultation/view-consultation-pacient',
+        component: ViewConsultationPatientComponent,
+    },
+    {
+        path: 'consultation/view-consultation-psychologist',
+        component: ViewConsultationPsychologistComponent,
+    },
+    {
         path: 'consultation/view-request-consultation',
-        component: MakeConsultationComponent,
+        component: ViewRequestConsultationComponent,
     },
     {
         path: 'pop-ups/pop-up-accept-consultation',
         component: PopUpAcceptConsultationComponent,
+    },
+    {
+        path: 'pop-ups/pop-up-cancel-consultation',
+        component: PopUpCancelConsultationComponent,
+    },
+    {
+        path: 'pop-ups/pop-up-give-feedback',
+        component: PopUpGiveFeedbackComponent,
+    },
+    {
+        path: 'pop-ups/pop-up-inform-availability',
+        component: PopUpInformAvailabilityComponent,
+    },
+    {
+        path: 'pop-ups/pop-up-make-consultation',
+        component: PopUpMakeConsultationComponent,
+    },
+    {
+        path: 'pop-ups/pop-up-reschedule-consultation',
+        component: PopUpRescheduleConsultationComponent,
     },
     {
         path: 'pop-ups/pop-up-refuse-consultation',

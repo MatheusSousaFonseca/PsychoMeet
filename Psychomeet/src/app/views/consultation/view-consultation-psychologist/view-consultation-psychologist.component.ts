d329cf310a5bccfcaa9ba7923f08f146b16d3060
karen <karenpereira9680@gmail.com>
2024-06-21 16:33:21 -0300

    feat: criacao de algumas telas

--- Psychomeet/src/app/views/consultation/view-consultation-psychologist/view-consultation-psychologist.component.ts
+++ Psychomeet/src/app/views/consultation/view-consultation-psychologist/view-consultation-psychologist.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-consultation-psychologist',
@@ -8,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './view-consultation-psychologist.component.css'
 })
 export class ViewConsultationPsychologistComponent {
+
+  constructor(private router: Router) { }
+
+  consultasPendentes() {
+    this.router.navigate(['consultation/view-request-consultation']);
+  }
+
+  agenda() {
+    this.router.navigate(['pop-ups/pop-up-inform-availability']);
+  }
 
 }
--- Psychomeet/src/app/views/consultation/view-request-consultation/view-request-consultation.component.ts
+++ Psychomeet/src/app/views/consultation/view-request-consultation/view-request-consultation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-view-request-consultation',
@@ -8,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrl: './view-request-consultation.component.css'
 })
 export class ViewRequestConsultationComponent {
+
+  constructor(private router: Router) { }
+
+  // consultas = [
+  //   { paciente: 'João Silva', data: '24/06/2024', hora: '16:00', descricao: 'Consulta de rotina' },
+  //   { paciente: 'Maria Oliveira', data: '25/06/2024', hora: '14:30', descricao: 'Sessão de terapia' },
+  //   { paciente: 'Carlos Souza', data: '26/06/2024', hora: '10:00', descricao: 'Consulta de acompanhamento' }
+  // ];
+
+  aceitarConsulta() {
+    this.router.navigate(['pop-ups/pop-up-accept-consultation']);
+
+
+  }
+
+  recusarConsulta() {
+    this.router.navigate(['pop-ups/pop-up-refuse-consultation']);
+
+  }
+
+  vizualizarConsulta() {
+    this.router.navigate(['consultation/view-consultation-psychologist']);
+
+  }
 
 }

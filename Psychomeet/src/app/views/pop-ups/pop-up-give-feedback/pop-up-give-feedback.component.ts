2a83dd2dd7a14017a900267467811b70f12dda0e
karen <karenpereira9680@gmail.com>
2024-06-21 21:31:21 -0300

    feat: edicao

--- Psychomeet/src/app/views/pop-ups/pop-up-give-feedback/pop-up-give-feedback.component.ts
+++ Psychomeet/src/app/views/pop-ups/pop-up-give-feedback/pop-up-give-feedback.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-pop-up-give-feedback',

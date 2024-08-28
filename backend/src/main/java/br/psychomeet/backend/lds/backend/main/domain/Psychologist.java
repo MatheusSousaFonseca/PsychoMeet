67209cf087acd342435b113708ca97c213fc6337
BrunoGabrielSI <brunomalaquias012345@gmail.com>
2024-08-28 15:43:55 -0300

    feat: criacao das classes do domain

---
+++ backend/src/main/java/br/psychomeet/backend/lds/backend/main/domain/Psychologist.java
@@ -0,0 +1,14 @@
+package br.psychomeet.backend.lds.backend.main.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Psychologist {
+
+    private int id;
+}

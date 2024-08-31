b92328d8070c3afd893b38722ee766ff641aefd8
bruno <brunomalaquias012345@gmail.com>
2024-08-31 14:31:41 -0300

    feat: correcao do consultation.component.ts

--- Psychomeet/src/app/views/app/consultation/view-scheduled-consultation/view-scheduled-consultation.component.ts
+++ Psychomeet/src/app/views/app/consultation/view-scheduled-consultation/view-scheduled-consultation.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { PsychologistReadService } from '../../../../services/psychologist/psychologist-read.service';
-import { ConsultationReadServiceService } from '../../../../services/consultation/consultation-read-service';
+import { ConsultationReadService } from '../../../../services/consultation/consultation-read-service';
 import { Psychologist } from '../../../../domain/model/psychologist-model';
 import { Consultation } from '../../../../domain/model/consultation-model';
 
@@ -24,7 +24,7 @@ export class ViewScheduledConsultationComponent {
 
 
 
-  constructor(private router: Router, private modalService: NgbModal, private consultationReadService: ConsultationReadServiceService) { }
+  constructor(private router: Router, private modalService: NgbModal, private consultationReadService: ConsultationReadService) { }
 
   ngOnInit(): void {
     this.loadConsultations();

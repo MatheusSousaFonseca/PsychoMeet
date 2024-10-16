90c8b9c01cb2112c7381bbbeb18c65e6f32ecaf4
matheus <matheusfonseca5100@gmail.com>
2024-10-15 21:59:13 -0300

    feat: edicao do perfil do paciente funcionando

--- Psychomeet/src/app/views/account/edit-profile-patient/edit-profile-patient.component.ts
+++ Psychomeet/src/app/views/account/edit-profile-patient/edit-profile-patient.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserUpdateService } from '../../../services/user/user-update-service';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-edit-profile-patient',
@@ -16,21 +17,24 @@ export class EditProfilePatientComponent {
 
   constructor(
     private router: Router,
-    private userUpdateService: UserUpdateService 
+    private userUpdateService: UserUpdateService,
+    public activeModal: NgbActiveModal 
   ) {}
 
   voltar() {
-    this.router.navigate(['account/my-profile-patient']);
+    this.activeModal.dismiss();
   }
 
   async salvar() {
     if (this.form.valid) {
       const updatedUser = this.form.value;
-    
+
       try {
-        
+        // Call the update service to save the user
         await this.userUpdateService.update(updatedUser);
         console.log("Usuário atualizado com sucesso!");
+
+        this.activeModal.close();  // Close the modal after a successful save
         this.router.navigate(['account/my-profile-patient']);
       } catch (error) {
         console.error("Erro ao atualizar o usuário:", error);
@@ -39,6 +43,5 @@ export class EditProfilePatientComponent {
       console.error("Formulário inválido");
     }
   }
-
-
 }
+
